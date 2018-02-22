ad8eb602127f0d0e479369188dd79ed5076d0f10
Ray Berry <rayberry@rays-macbook>
2018-02-22 15:42:05 +0000

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Ray Berry on 22/02/2018.
+//  Copyright © 2018 JARBerry. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+}
